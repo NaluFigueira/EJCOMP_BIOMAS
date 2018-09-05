1c278bf18860a8439758027966039ebf54f1305c
weslley543 <30299972+weslley543@users.noreply.github.com>
2018-09-05 17:51:10 -0300

    mechi no JS

--- assets/js/funcoes.js
+++ assets/js/funcoes.js
@@ -14,3 +14,15 @@ $(document).ready(function(){
   $(document).ready(function(){
     $('.slider').slider();
 });
+
+ $(document).ready(function(){
+ 	$('#Servicos').click(function(){
+ 		if($("#subservicos").is(':visible')){
+ 			$("#subservicos").hide("slow")
+ 		}else{
+ 			$("#subservicos").show("slow")
+ 		}
+ 	});
+ 	
+ 	
+ });
